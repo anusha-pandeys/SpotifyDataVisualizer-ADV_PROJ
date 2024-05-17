bc04dc876d93f30ebad4dc76433c48525695c84b
s-anpandey <113121493+s-anpandey@users.noreply.github.com>
2024-05-16 19:28:03 -0700

    refresh login, added logout page

--- spotify-project/client/src/App.js
+++ spotify-project/client/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { accessToken, logout } from './spotify';
 import './App.css';
-import { useEffect } from 'react';
 
 function App() {
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
+    setToken(accessToken);
   }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -33,7 +45,4 @@ function App() {
     </div>
   );
 }
-
-
-
-export default App;
+*/
