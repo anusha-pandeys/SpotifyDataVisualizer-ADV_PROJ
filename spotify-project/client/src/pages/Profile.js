fd6227891238639750df91d9ec9a2024830af202
s-anpandey <113121493+s-anpandey@users.noreply.github.com>
2024-05-26 17:03:14 -0700

    profile header: blank photo & follower count

--- spotify-project/client/src/pages/Profile.js
+++ spotify-project/client/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { accessToken, getCurrentUserProfile } from '../spotify';
-
-
+import { StyledHeader } from '../styles';
+import blank_profile from './blank_profile.jpg'; 
  
 const Profile = () => {
     const [token, setToken] = useState(null);
@@ -29,13 +29,26 @@ const Profile = () => {
   return (
     <>
       {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} Followers</p>
-          {profile.images.length && profile.images[0].url && (
-            <img src={profile.images[0].url} alt="Avatar"/>
-          )}
-        </div>
+        <>
+          <StyledHeader type="user">
+            <div className="header__inner">
+            <img
+                className="header__img"
+                src={profile.images && profile.images.length > 0 && profile.images[0].url ? profile.images[0].url : blank_profile}
+                alt="Avatar"
+              />
+              <div>
+                <div className="header__overline">Profile</div>
+                <h1 className="header__name">{profile.display_name}</h1>
+                <p className="header__meta">
+                  <span>
+                    {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+        </>
       )}
     </>
   )
